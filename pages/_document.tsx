ce7e17e0ddd34096f79652a5726c5eddf4bb3cb4
Yvens16 <yvensbelaston@gmail.com>
2022-01-25 14:58:32 +0100

    started

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,17 +11,17 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-              <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;800&display=swap" rel="stylesheet"/>
-              </Head>
-              <body className='font-poppins text-base bg-default_bg'>
-                <Main />
-                <NextScript />
-              </body>
-            </Html>
-            )
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;800&display=swap" rel="stylesheet" />
+        </Head>
+        <body className='font-poppins text-base bg-default_bg'>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
 
-            export default MyDocument
+export default MyDocument
---
+++ pages/personal_space.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import type { NextPage } from 'next'
+
+const PersonalSpace: NextPage = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
+
+export default PersonalSpace;
