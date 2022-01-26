3da47a8a26c034c49f6692b630ce4d37f75601a1
Yvens16 <yvensbelaston@gmail.com>
2022-01-26 16:47:10 +0100

    test works but why do I need to use production credentials ?

--- firebase/index.ts
+++ firebase/index.ts
@@ -2,29 +2,6 @@ import { FirebaseOptions, initializeApp, getApp } from "firebase/app";
 import { connectFirestoreEmulator, getFirestore, setDoc, doc, Firestore } from "firebase/firestore";
 import { getAuth, connectAuthEmulator, Auth } from "firebase/auth";
 
-interface IaddUserInfo {
-  uid: string,
-  firstName: string,
-  lastName: string,
-  howDoYouKnowUs: string,
-  email: string,
-}
-
-const addUserInfo = async ({ uid, firstName, lastName, howDoYouKnowUs, email }: IaddUserInfo) => {
-  try {
-    await setDoc(doc(firestore, 'users', uid), {
-      uid,
-      firstName,
-      lastName,
-      howDoYouKnowUs,
-      email
-    });
-  } catch (e) {
-    // TODO: add snackbar to display the error here
-    console.log('Error in addUserInfo to firestore', e);
-  }
-}
-
 const firebaseCredentials: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
