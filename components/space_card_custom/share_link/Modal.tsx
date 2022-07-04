2e6f4c3b6c75db8f082da0e7a0a4907d3cfc680a
Yvens16 <yvensbelaston@gmail.com>
2022-07-04 23:35:53 +0200

    right size shareModal

--- components/space_card_custom/share_link/Modal.tsx
+++ components/space_card_custom/share_link/Modal.tsx
@@ -8,7 +8,7 @@ function Modal({ url, show, closeModal }: IShareLinkModal) {
     navigator.clipboard.writeText(textToCopy);
   }
   return (
-    <SimpleModal show={show} closeModal={closeModal} titleHtml={undefined} blurClass="backdrop-blur-[7px]">
+    <SimpleModal show={show} closeModal={closeModal} titleHtml={undefined} customClass="xl:top-[88px] xl:-translate-y-0 xl:w-[423px]" blurClass="backdrop-blur-[7px]">
       <>
         <div className="header flex justify-between mb-16t">
           <h3 className="text-medium text-18t">Votre lien de partage</h3>
